0d19d6d2c9fda027315734a0d08256e5da2284b9
mrjzhang <mrjzhang@tencent.com>
2018-10-12 22:48:46 +0800

    :rocket: force update commits json

---
+++ command/update-commit.js
@@ -0,0 +1,18 @@
+'use strict'
+const logger = require('../src/logger')
+const { getToken } = require('../src/token')
+const { readFilePromise } = require('../src/oper-file')
+const { parser } = require('../src/commits')
+
+module.exports = async () => {
+  const userInfo = await getToken()
+  // console.log('userInfo', userInfo)
+
+  const username = userInfo.name
+  if (!username) {
+    logger.fail('you do not have a username of github.')
+    return
+  }
+
+  await parser(username)
+}
